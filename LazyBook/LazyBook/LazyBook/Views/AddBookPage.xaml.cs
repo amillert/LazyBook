6e972de853f902b26e0c02ca8a0d8adf4f430a65
Albert Millert <albertmillert97@gmail.com>
2017-10-21 23:05:59 +0200

    Working on AddBookPage

---
+++ LazyBook/LazyBook/LazyBook/Views/AddBookPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LazyBook.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddBookPage : ContentPage
+	{
+        public AddBookPage()
+        {
+            //IEnumerable
+            //foreach (var value in Enum.GetValues(typeof(Category)))
+            //{
+            //    value.ToString();
+            //}
+
+            InitializeComponent();
+
+            foreach(string cat in Category)
+            {
+                categories.Item.Add();
+            }
+        }
+	}
+}
