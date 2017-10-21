aae2939baefa9ae6a2b86c1b8406eac8b2af2026
Albert Millert <albertmillert97@gmail.com>
2017-10-21 19:14:21 +0200

    Changed colors + master detail page + Started vaildation

--- LazyBook/LazyBook/LazyBook/Views/LoggingView.xaml.cs
+++ LazyBook/LazyBook/LazyBook/Views/LoggingView.xaml.cs
@@ -38,7 +38,30 @@ namespace LazyBook.Views
 
         private void LogIn_Clicked(object sender, EventArgs e)
         {
+        //    bool Success = true;
+            
+        //    for(var user in Users)
+        //    {
+        //        if (user.username == usernameEntry.Text)
+        //        {
+        //            DisplayAlert("Registration", "Username is already in use!", "OK");
+        //            Success = false;
+        //            break;
+        //        }
+        //        if (user.email == emailEntry.Text)
+        //        {
+        //            DisplayAlert("Registratiion", "E-mail is already in use!", "OK");
+        //            Success = false;
+        //            break;
+        //        }
+        //    }
 
-        }
+        //    if (passwordEntry != password)
+        //    {
+        //        DisplayAlert("Registration", "Your password doesn't match confirmation!", "OK");
+        //        Success = false;
+        //        user.password == passwordEntry.Text
+        //    }
+        //}
     }
 }
---
+++ LazyBook/LazyBook/LazyBook/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LazyBook.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //bool Success = true;
+            
+            //for(var user in Users)
+            //{
+            //    if (user.username == usernameEntry.Text)
+            //    {
+            //        DisplayAlert("Registration", "Username is already in use!", "OK");
+            //        Success = false;
+            //        break;
+            //    }
+            //    if (user.email == emailEntry.Text)
+            //    {
+            //        DisplayAlert("Registratiion", "E-mail is already in use!", "OK");
+            //        Success = false;
+            //        break;
+            //    }
+            //}
+
+            if (passwordEntry != password2Entry)
+            {
+                DisplayAlert("Registration", "Your passwords don't match!", "OK");
+                Success = false;
+                user.password == passwordEntry.Text;
+            }
+
+            DisplayAlert("Registeration", "Registration completed successfully!", "OK");
+        }
+    }
+}
