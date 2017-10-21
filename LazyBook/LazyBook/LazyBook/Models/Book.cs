a3f284f1783b4f9c48454a39e2f19338bc8674c7
AleksMarzec <aleks.marzec@gmail.com>
2017-10-21 17:36:11 +0200

    Added protype of Book class.

--- LazyBook/LazyBook/LazyBook/Models/Book.cs
+++ LazyBook/LazyBook/LazyBook/Models/Book.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+enum Category { Business, Cooking, History, Computers, Detective, Thriller, Kids, Politics, Law, Religion, Romance, Science_Fiction, Health} 
+
 namespace LazyBook.Models
 {
     class Book
     {
+        private string name;
+        private string author;
+        private string publisher;
+        private int year;
+        private Category category;
+        private string summary;
 
+        public Book(string name, string author, string publisher, int year, Category category, string summary)
+        {
+            this.name = name;
+            this.author = author;
+            this.publisher = publisher;
+            this.year = year;
+            this.category = category;
+            this.summary = summary;
+        }
     }
 }
