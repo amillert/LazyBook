a0a696f584a33859860086ea9511f267b62170b4
Bartosz Mikulski <bartmiki97@gmail.com>
2017-10-22 09:22:16 +0200

    Splash Screen

--- LazyBook/LazyBook/LazyBook.Android/SplashActivity.cs
+++ LazyBook/LazyBook/LazyBook.Android/SplashActivity.cs
@@ -5,14 +5,13 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Util;
 using LazyBook.Droid;
-using static Android.Resource;
 
 namespace com.xamarin.sample.splashscreen
 {
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : AppCompatActivity
     {
-        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+        static readonly string TAG = "X:" + typeof (SplashActivity).Name;
 
         public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
         {
@@ -32,12 +31,12 @@ namespace com.xamarin.sample.splashscreen
         public override void OnBackPressed() { }
 
         // Simulates background work that happens behind the splash screen
-        async void SimulateStartup()
+        async void SimulateStartup ()
         {
             Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
-            //await Task.Delay(100); // Simulate a bit of startup work.
+           // await Task.Delay(8000); // Simulate a bit of startup work.
             Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
-            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            StartActivity(new Intent(Application.Context, typeof (MainActivity)));
         }
     }
 }
