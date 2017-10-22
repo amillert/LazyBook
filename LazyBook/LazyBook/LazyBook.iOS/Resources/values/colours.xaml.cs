b8e87c3211ee2623a72b79d30ef51caaa8c80e90
Micha Mironczuk <michal4010@wp.pl>
2017-10-22 08:39:19 +0200

    Added SplashScreen - Miron

---
+++ LazyBook/LazyBook/LazyBook.iOS/Resources/values/colours.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LazyBook.iOS.Resources.values
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class colours : ContentView
+    {
+        public colours()
+        {
+            InitializeComponent();
+        }
+    }
+}
