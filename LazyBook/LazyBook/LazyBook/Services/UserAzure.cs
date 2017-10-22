9c523d738bce43dfbe44cd7210376e4e638d26b0
Bartosz Mikulski <bartmiki97@gmail.com>
2017-10-22 03:33:41 +0200

    Complete User, tweaks in Item

--- LazyBook/LazyBook/LazyBook/Services/UserAzure.cs
+++ LazyBook/LazyBook/LazyBook/Services/UserAzure.cs
@@ -14,7 +14,109 @@ using LazyBook.Models;
 namespace LazyBook.Services
 {
     class UserAzure : IDataStore<User>
-
     {
+        public MobileServiceClient Client { get; set; } = null;
+        IMobileServiceSyncTable<User> table;
+        List<User> users;
+
+        public async Task Initialize()
+        {
+            if (Client?.SyncContext?.IsInitialized ?? false)
+                return;
+
+            var appUrl = "http://lazybook.azurewebsites.net";
+
+            Client = new MobileServiceClient(appUrl);
+
+            var path = "syncstore_users.db";
+            path = Path.Combine(MobileServiceClient.DefaultDatabasePath, path);
+
+            //tworzenie lokalnej bazy danych
+            var store = new MobileServiceSQLiteStore(path);
+            //definiowanie tabeli
+            store.DefineTable<Item>();
+
+            //inicjalizownie synchronizacji
+            await Client.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
+
+            //tabela polaczona z Azure
+            table = Client.GetSyncTable<User>();
+            users = new List<User>();
+        }
+
+        public async Task SyncBooks()
+        {
+            try
+            {
+                await table.PullAsync("allUsers", table.CreateQuery());
+
+                await Client.SyncContext.PushAsync();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Unable to sync users " + e);
+            }
+
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            await Initialize();
+            await SyncBooks();
+
+            return await table.OrderBy(s => s.UserName).ToEnumerableAsync();
+
+        }
+
+        public async Task<bool> UserExist(string email)
+        {
+            return users.Exists((User u) => u.Email == email);
+        }
+
+        public async Task<User> AddUser(User u)
+        {
+            await table.InsertAsync(u);
+            //Synchronize Users
+            await SyncBooks();
+            return u;
+        }
+
+        public UserAzure()
+        {
+            users = new List<User>();
+        }
+
+        public async Task<bool> AddItemAsync(User user)
+        {
+            users.Add(user);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(User user)
+        {
+            var _item = users.Where((User arg) => arg.Id == user.Id).FirstOrDefault();
+            users.Remove(_item);
+            users.Add(user);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var _item = users.Where((User arg) => arg.Id == id).FirstOrDefault();
+            users.Remove(_item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<User> GetItemAsync(string id)
+        {
+            return await Task.FromResult(users.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<User>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(users);
+        }
     }
 }
---
+++ LazyBook/LazyBook/LazyBook/ViewModels/UserViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+using LazyBook.Models;
+using LazyBook.Services;
+
+namespace LazyBook.ViewModels
+{
+    class UserViewModel
+    {
+        UserAzure azureServices;
+        public ObservableCollection<User> Users { get; set; }
+        public Command LoadUsersCommand { get; set; }
+        User User { get; set; }
+        public bool IsBusy { get; set; }
+
+        public UserViewModel()
+        {
+            azureServices = DependencyService.Get<UserAzure>();
+            Users = new ObservableCollection<User>();
+            LoadUsersCommand = new Command(async () => await ExecuteLoadUsersCommand());
+        }
+
+        public async Task ExecuteLoadUsersCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Users.Clear();
+                var users = await azureServices.GetUsers();
+                foreach (var user in users)
+                {
+                    Users.Add(user);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("UserViewModel LoadUser Failure: " + ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public async Task ExecuteAddUserCommand(User user)
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                User = user;
+                var item = await azureServices.AddUser(User);
+                Users.Add(user);
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("UserViewModel AddUser Failure: " + ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
