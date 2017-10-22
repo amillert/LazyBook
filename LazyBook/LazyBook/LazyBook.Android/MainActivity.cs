56dcf80ddd02dccb5b1021694df76a98129b9af6
AleksMarzec <aleks.marzec@gmail.com>
2017-10-22 02:24:31 +0200

    Just changed app name

--- LazyBook/LazyBook/LazyBook.Android/MainActivity.cs
+++ LazyBook/LazyBook/LazyBook.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace LazyBook.Droid
 {
-    [Activity(Label = "LazyBook.Android", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "LazyBook", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
